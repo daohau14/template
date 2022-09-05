0240eb688a4e176a4932216986d2590e84e1d7b7
daohau14 <daohau140200@gmail.com>
2022-09-05 10:42:45 +0700

    upload

--- add_to_cart.js
+++ add_to_cart.js
@@ -1,3 +1,63 @@
+const products = [
+    { 
+      id: 0,
+      img: './assets/img/product1.jpg',
+      name: 'Supendisse id volutpat',
+      price: 84,
+      inCart: 0
+    },
+    {
+      id: 1,
+      img: './assets/img/product2.jpg',
+      name: 'Body Solid GCEC340',
+      price: 214,
+      inCart: 0
+    },
+    {
+      id: 2,
+      img: './assets/img/product3.jpg',
+      name: 'Bowflex BXE116 Elliptical',
+      price: 421,
+      inCart: 0
+    },
+    {
+      id: 3,
+      img: './assets/img/product4.jpg',
+      name: 'ETHOS GHD',
+      price: 152,
+      inCart: 0
+  
+    },
+    {
+      id: 4,
+      img: './assets/img/product5.jpg',
+      name: 'AFG Pro 7.2AI Incline',
+      price: 252,
+      inCart: 0
+    },
+    {
+      id: 5,
+      img: './assets/img/product6.jpg',
+      name: 'Weight Bench',
+      price: 412,
+      inCart: 0
+    },
+    {
+      id: 6,
+      img: './assets/img/product7.jpg',
+      name: 'ProForm Hiit Trainer Lite 5.9',
+      price: 300,
+      inCart: 0
+    },
+    {
+      id: 7,
+      img: './assets/img/product8.jpg',
+      name: 'Fitness Gear Pro',
+      price: 124,
+      inCart: 0
+    }
+  ];
+  
 
 //--------------Thêm giỏ hàng-------------------------------------//
 
@@ -21,10 +81,11 @@ function  addCart(productName, productImg, productPrice){
     var cartItem = document.querySelectorAll(".item tr")
     for (var i = 0; i < cartItem.length; i++){
         var productItem = document.querySelectorAll(".heading")
-        // if(productItem[i].innerHTML == productName ){
-        //     alert('Sản phẩm bạn chọn đã có trong giỏ hàng!!!')
-        //     return
-        // }
+        if(productItem[i].innerHTML == productName ){
+            // alert('Sản phẩm bạn chọn đã có trong giỏ hàng!!!')
+            // return {
+            // }
+        }
     }
     var trcontent = '<td><img src="'+productImg+'" alt="Supendisse id volutpat" class="cart-ads1"></td> <td> <h3 class="heading">'+productName+'</h3></td> <td><p>$<span class="price">'+productPrice+'</span></p></td> <td><input type="number" aria-label="" value="1" min="1"></td> <td><span class= "btn-del-item-cart">Xoá</span></td>'
     addItem.innerHTML = trcontent;
